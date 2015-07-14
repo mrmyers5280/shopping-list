ce1b7434b9fa00da8db7a545befd255288160bb4
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-14 15:26:29 -0600

    Add js to toggle checkbox on and off. Closes #4

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 // app.js
+$(document).ready(function() {
+    $('.shopping-list li').mousedown(function() {
+        // console.log('mousedown li')
+        $(this).toggleClass('checked');
+    });
+});
