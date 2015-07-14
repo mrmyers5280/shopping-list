432bfa511913f038d4cfd2529ed0623f616f7558
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-14 17:05:40 -0600

    With app.js, add code to append a new list item

--- js/app.js
+++ js/app.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
         // console.log('mousedown li')
         $(this).toggleClass('checked');
     });
+    // append user input as new item in list
+    $('#add-list-item').keydown(function(event) {
+        // console.log('keypress: ' + event.which);
+        if (event.which == 13) {
+            $('.shopping-list').append('<li>' + $('#add-list-item').val() + '</li>');
+        }
+    });
+    // allow user to remove and item from the list
 });
