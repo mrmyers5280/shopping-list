b50853719dae35250415ad1c64544e6d79843d82
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-15 17:37:34 -0600

    Add if statement to trap for empty text input

--- js/app.js
+++ js/app.js
@@ -7,8 +7,13 @@ $(document).ready(function() {
     // append user input as new item in list
     $('#add-list-item').keyup(function(event) {
         // console.log('keypress: ' + event.which);
-        if (event.which == 13) {
-            $('.shopping-list').append('<li>' + $('#add-list-item').val() + '</li>');
+        if ($('#add-list-item').val() != '') {
+            if (event.which == 13) {
+                $('.shopping-list').append('<li>' + $('#add-list-item').val() + '</li>');
+                $('#add-list-item').val('');
+            }
+        } else {
+            alert('Please enter a value before trying to add an item.');
         }
     });
     // allow user to remove and item from the list
