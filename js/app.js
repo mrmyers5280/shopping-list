8a759276223001549ad7524b79e94e22b9cfabdf
mrmyers5280 <not.that.mike.myers@gmail.com>
2016-04-01 18:07:25 -0600

    Add function to increase red lines height when a new item is added

--- js/app.js
+++ js/app.js
@@ -8,34 +8,42 @@
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // app.js
 $(document).ready(function() {
-    $('.shopping-list').on('click', 'li', function() {
-        // console.log('mousedown li')
-        if ($(this).attr('aria-checked') === '') {
-            //uncheck
-            $(this).removeAttr('aria-checked');
-        } else {
-            //check the box
-            $(this).attr('aria-checked', '');
-        }
-    });
-    // append user input as new item in list
-    $('#add-list-item').keyup(function(event) {
-        // console.log('keypress: ' + event.which);
-        if ($('#add-list-item').val() != '') {
-            if (event.which == 13) {
-                $('.shopping-list').append('<li>' + $('#add-list-item').val() + '<span></span></li>');
-                $('#add-list-item').val('');
-            }
-        } else {
-            alert('Please enter a value before trying to add an item.');
-        }
-    });
-    // allow user to remove an item from the list
-    $('.shopping-list').on('click', 'li span', function() {
-        //console.log(this.closest('li'));
-        $(this).closest('li').remove();
-    });
-    // Allow user to drag items to re-order them
-    $('.shopping-list').sortable();
-    $('.shopping-list').disableSelection();
+	$('.shopping-list').on('click', 'li', function() {
+		// console.log('mousedown li')
+		if ($(this).attr('aria-checked') === '') {
+			//uncheck
+			$(this).removeAttr('aria-checked');
+		} else {
+			//check the box
+			$(this).attr('aria-checked', '');
+		}
+	});
+	// append user input as new item in list
+	$('#add-list-item').keyup(function(event) {
+		// console.log('keypress: ' + event.which);
+		if ($('#add-list-item').val() != '') {
+			if (event.which == 13) {
+				$('.shopping-list').append('<li>' + $('#add-list-item').val() + '<span></span></li>');
+				$('#add-list-item').val('');
+				$(".lines").css("height", newHeight);
+			}
+		} else {
+			alert('Please enter a value before trying to add an item.');
+		}
+	});
+	// allow user to remove an item from the list
+	$('.shopping-list').on('click', 'li span', function() {
+		//console.log(this.closest('li'));
+		$(this).closest('li').remove();
+	});
+	// Allow user to drag items to re-order them
+	$('.shopping-list').sortable();
+	$('.shopping-list').disableSelection();
+
+	var newHeight = function() {
+		var x = $(".lines").css("height");
+		var y = parseInt(x) + 43;
+		x = y + "px";
+		return x;
+	};
 });
