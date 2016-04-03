cab07a418c91d980e2bb2e3a5bf03d1c1a50fc1e
mrmyers5280 <not.that.mike.myers@gmail.com>
2016-04-02 20:41:53 -0600

    Add functions to add and remove red lines; replace jQuery calls with variables

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // app.js
 $(document).ready(function() {
-	$('.shopping-list').on('click', 'li', function() {
+	var shoppingList = $('.shopping-list');
+	var lines = $('.lines');
+	shoppingList.on('click', 'li', function() {
 		// console.log('mousedown li')
 		if ($(this).attr('aria-checked') === '') {
 			//uncheck
@@ -23,26 +25,33 @@ $(document).ready(function() {
 		// console.log('keypress: ' + event.which);
 		if ($('#add-list-item').val() != '') {
 			if (event.which == 13) {
-				$('.shopping-list').append('<li>' + $('#add-list-item').val() + '<span></span></li>');
+				shoppingList.append('<li>' + $('#add-list-item').val() + '<span></span></li>');
 				$('#add-list-item').val('');
-				$(".lines").css("height", newHeight);
+				lines.css('height', addHeight());
 			}
 		} else {
 			alert('Please enter a value before trying to add an item.');
 		}
 	});
 	// allow user to remove an item from the list
-	$('.shopping-list').on('click', 'li span', function() {
+	shoppingList.on('click', 'li span', function() {
 		//console.log(this.closest('li'));
 		$(this).closest('li').remove();
+		lines.css('height', removeHeight());
 	});
 	// Allow user to drag items to re-order them
-	$('.shopping-list').sortable();
-	$('.shopping-list').disableSelection();
+	shoppingList.sortable();
+	shoppingList.disableSelection();
 
-	var newHeight = function() {
-		var x = $(".lines").css("height");
+	var addHeight = function() {
+		var x = lines.css('height');
 		var y = parseInt(x) + 43;
+		x = y + "px";
+		return x;
+	};
+	var removeHeight = function() {
+		var x = lines.css('height');
+		var y = parseInt(x) - 43;
 		x = y + "px";
 		return x;
 	};
